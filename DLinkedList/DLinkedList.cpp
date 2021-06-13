e3fafb9a947ad782c38376fec45d5e77e2627801
grace_goose <choieungi@gm.gist.ac.kr>
2021-06-13 21:55:24 +0900

    DLinkedList

---
+++ DLinkedList/DLinkedList.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <stdexcept> // for std::out_of_range
+
+using namespace std;
+
+typedef string Elem;				// list element type
+class DNode {					// doubly linked list node
+private:
+    Elem elem;					// node element value
+    DNode* prev;				// previous node in list
+    DNode* next;				// next node in list
+    friend class DLinkedList;			// allow DLinkedList access
+};
+
+class DLinkedList {				// doubly linked list
+public:
+    DLinkedList();				// constructor
+    ~DLinkedList();				// destructor
+    bool empty() const;				// is list empty?
+    const Elem& front() const;			// get front element
+    const Elem& back() const;			// get back element
+    void addFront(const Elem& e);		// add to front of list
+    void addBack(const Elem& e);		// add to back of list
+    void removeFront();				// remove from front
+    void removeBack();				// remove from back
+    void printList();
+private:					// local type definitions
+    DNode* header;				// list sentinels
+    DNode* trailer;
+protected:					// local utilities
+    void add(DNode* v, const Elem& e);		// insert new node before v
+    void remove(DNode* v);			// remove node v
+};
+
+bool DLinkedList::empty() const		// is list empty?
+{ return (header->next == trailer); }
+
+const Elem& DLinkedList::front() const	// get front element
+{ return header->next->elem; }
+
+const Elem& DLinkedList::back() const		// get back element
+{ return trailer->prev->elem; }
+
+DLinkedList::DLinkedList() {			// constructor
+    header = new DNode;
+    trailer = new DNode;
+    header->next = trailer;			// have them point to each other
+    trailer->prev = header;
+}
+
+DLinkedList::~DLinkedList() {			// destructor
+    while (!empty()) removeFront();		// remove all but sentinels
+    delete header;				// remove the sentinels
+    delete trailer;
+    cout << "Destructor!" << endl;
+}
+void DLinkedList::printList(){
+    DNode* dlp;
+    dlp = header;
+    while(dlp != trailer) {
+        cout << dlp->elem << " - ";
+        dlp = dlp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void DLinkedList::add(DNode* v, const Elem& e) {
+    DNode* u = new DNode;u->elem = e;		// create a new node for e
+    u->next = v;				// link u in between v
+    u->prev = v->prev;				// ...and v->prev
+    v->prev->next = u;
+    v->prev = u;
+}
+
+void DLinkedList::addFront(const Elem& e)	// add to front of list
+{ add(header->next, e); }
+
+void DLinkedList::addBack(const Elem& e)	// add to back of list
+{ add(trailer, e); }
+
+void DLinkedList::remove(DNode* v) {		// remove node v
+    DNode* u = v->prev;				// predecessor
+    DNode* w = v->next;				// successor
+    u->next = w;				// unlink v from list
+    w->prev = u;
+    delete v;
+}
+
+void DLinkedList::removeFront()		// remove from font
+{ remove(header->next); }
+
+void DLinkedList::removeBack()		// remove from back
+{ remove(trailer->prev); }
+
+
+int main(){
+    DLinkedList ddl;
+    ddl.addFront("hello");
+    ddl.addFront("hi");
+    ddl.printList();
+}
+
+//template 구현으로 변경하기
--- Data Structure (C++  Java  Python ).md
+++ Data Structure (C++  Java  Python ).md
@@ -24,7 +24,35 @@
 - size : O(1)
 - empty O(1)
 
-[Code](/ArrayList/ArrayList.cpp)
+[Code(C++)](/ArrayList/ArrayList.cpp)
 
 ## Linked List 
 
+### Single Linked List 
+
+> A singly linked list is a concrete data structure consisting of a sequence of nodes. Each node stores element and link to the next node 
+
+- sequential access is slow because elements not in continuous memory locations.
+- When you have to perform more search operation than that of insert or delete operation than it’s best to go ahead with an array list but in reverse case it’s always better to go ahead with linked list.
+
+**Main method**
+
+- search : O(n)
+- insert, delete :O(1)
+
+**Usage**
+
+1. The time-sharing problem used by the scheduler during the scheduling of the processes in the Operating system.
+2. *Image viewer* – Previous and next images are linked, hence can be accessed by the next and previous button.
+
+
+
+### Double Linked List
+
+**Main method** 
+
+- addFront, addBack, add, removeFront, removeBack, remove() : O(1)
+- front(), back(), empty() : O(1)
+- `DNode* header` , `Dnode* trailer` : private pointer 
+
+[Code(C++)](/DLinkedList/DLinkedList.cpp)
